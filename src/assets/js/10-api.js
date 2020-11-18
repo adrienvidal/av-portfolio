9b7b04f654d74ae848b12b7d68e27dd55e68555d
Adrien Vidal <adriensg.vidal@gmail.com>
2020-11-18 23:39:24 +0100

    clean api call

--- src/assets/js/10-api.js
+++ src/assets/js/10-api.js
@@ -4,28 +4,17 @@ class Api {
   }
 
   async getSkills() {
-    const skillsResponse = await fetch(this.rootPath + "public/api/skillsData.json");
-
-    const skills = await skillsResponse.json();
-
-    return skills;
+    const res = await fetch(this.rootPath + 'public/api/skillsData.json');
+    return await res.json();
   }
 
   async getPortfolio() {
-    const portfolioResponse = await fetch(
-      this.rootPath + "public/api/portfolioData.json"
-    );
-
-    const portfolio = await portfolioResponse.json();
-
-    return portfolio;
+    const res = await fetch(this.rootPath + 'public/api/portfolioData.json');
+    return await res.json();
   }
 
   async getSocials() {
-    const socialsResponse = await fetch(this.rootPath + "public/api/socialData.json");
-
-    const socials = await socialsResponse.json();
-
-    return socials;
+    const res = await fetch(this.rootPath + 'public/api/socialData.json');
+    return await res.json();
   }
 }
