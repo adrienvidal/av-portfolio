2246c3146b38a364a9548705bc494ba19b067f70
Adrien Vidal <adriensg.vidal@gmail.com>
2025-02-02 14:54:06 +0100

    refacto

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './' // Assure que les chemins sont relatifs au dossier où `index.html` est servi
+  // Autres configurations...
+})
